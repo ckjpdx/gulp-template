e75df96d9047a0a342ab78466a6b66404808af46
Chris K Johnson and Ginger Lee Kretschmer <chris@>
2018-01-03 09:16:40 -0800

    add stuff

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,13 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+  .pipe(concat('allConcat.js'))
+  .pipe(gulp.dest('./tmp'));
+});
 
 gulp.task('jsBrowserify', ['concatInterface'], function() {
   return browserify({ entries: ['./js/*-interface.js'] })
@@ -11,8 +17,8 @@ gulp.task('jsBrowserify', ['concatInterface'], function() {
     .pipe(gulp.dest('./build/js'));
 });
 
-gulp.task('concatInterface', function() {
-  return gulp.src(['./js/*-interface.js'])
-    .pipe(concat('allConcat.js'))
-    .pipe(gulp.dest('./tmp'));
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
 });
